4cbcccf85ed4086168f896f45f93792d950068a2
Javier <javico2609@gmail.com>
2019-03-25 18:38:16 -0300

    chore: add greenery-clone

--- lib/pages/templates/greenery-clone/greenery-home.dart
+++ lib/pages/templates/greenery-clone/greenery-home.dart
@@ -5,11 +5,144 @@ class GreeneryHome extends StatefulWidget {
   _GreeneryHomeState createState() => _GreeneryHomeState();
 }
 
+var greenColor = Color(0xff32a05f);
+var darkGreenColor = Color(0xff279152);
+var productImage =
+    'https://i.pinimg.com/originals/8f/bf/44/8fbf441fa92b29ebd0f324effbd4e616.png';
+
 class _GreeneryHomeState extends State<GreeneryHome> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: greenColor,
+      appBar: AppBar(
+        leading: Icon(
+          Icons.arrow_back,
+          color: Colors.black,
+        ),
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            flex: 4,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.only(bottomLeft: Radius.circular(110))),
+            ),
+          ),
+          buildBottomSection(context)
+        ],
+      ),
+    );
+  }
+
+  Widget buildBottomSection(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 35.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 15),
+            Text(
+              "Planting",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  height: 90,
+                  width: MediaQuery.of(context).size.width / 2 - 50,
+                  decoration: BoxDecoration(
+                    color: darkGreenColor,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: <Widget>[
+                          Text(
+                            "250",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            "ml",
+                            style: TextStyle(color: Colors.white70),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "water",
+                        style: TextStyle(color: Colors.white70),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 90,
+                  width: MediaQuery.of(context).size.width / 2 - 50,
+                  decoration: BoxDecoration(
+                    color: darkGreenColor,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: <Widget>[
+                          Text(
+                            "+ 18",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            "ºC",
+                            style: TextStyle(color: Colors.white70),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "Sunshine",
+                        style: TextStyle(color: Colors.white70),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
